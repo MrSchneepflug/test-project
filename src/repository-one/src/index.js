52174cb382e8c56cb59cb973c5654de8ee9f4b98
Johannes Jambor <johannes.jambor@rewe-digital.com>
2019-06-07 10:47:06 +0200

    fix

--- src/repository-one/src/index.js
+++ src/repository-one/src/index.js
@@ -1,7 +1,6 @@
 console.log("repository-one");
 
-
-function( string ) {
+function test( string ) {
   var emailExpression = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
  return emailExpression.test( string );
