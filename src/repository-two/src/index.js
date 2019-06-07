5cddbd8338eb9ebb9306d31505ba17c3e2322e99
Johannes Jambor <johannes.jambor@rewe-digital.com>
2019-06-07 10:50:52 +0200

    add vulnerability

--- src/repository-two/src/index.js
+++ src/repository-two/src/index.js
@@ -1,1 +1,11 @@
 console.log("repository-two");
+
+var user = function() {
+    this.name = 'jon';
+    //An empty user constructor.
+};
+
+function handler(userInput) {
+    var anyVal = 'anyVal'; // This can be any attribute, and does not need to be user controlled.
+    user[anyVal] = user[userInput[0]](userInput[1]);
+}
